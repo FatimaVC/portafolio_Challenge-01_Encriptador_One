eee14756662b26b4ad6b517da75da486fc30303d
Fatima <fvillegasch@unprg.edu.pe>
2023-01-07 12:35:06 -0500

    transpasando archivos

---
+++ main.js
@@ -0,0 +1,33 @@
+function encriptar(){
+    var texto = document.getElementById("inputText").value;
+    var mayusculas = /[A-Z]/g;
+    var caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:."`;.,áéíóúàèìòù']/g;
+ 
+     if(texto == ""){
+ 
+         alert("No has ingresado nada para encriptar");
+     }
+     else if(texto.match(mayusculas) != texto.match(mayusculas)){
+ 
+         alert("No puede ingresar letras mayúsculas");
+     }
+     else if(texto.match(caracteres) != texto.match(caracteres)){
+ 
+         alert("No puede ingresar carácteres");
+ 
+     }else{
+         var txtCifrado = texto.replace(/e/igm, "enter");
+         var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+         var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+         var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+         var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
+ 
+ 
+         document.getElementById("imgRight").style.display = "none";
+         document.getElementById("texto").style.display = "none";
+         document.getElementById("texto1").innerHTML = txtCifrado;
+         document.getElementById("copiar").style.display = "show";
+         document.getElementById("copiar").style.display = "inherit";
+     }
+ }
+ 
